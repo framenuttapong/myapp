a362f61ffaa32cd9e019b6af4edd030ca7593eb3
Nuttapong Nadee <nuttapong.n@ku.th>
2022-07-04 01:51:18 +0700

    initial commit : add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,11 @@
+package myapp;
+
+public class Cat {
+    String name;
+    String type;
+
+    public Cat(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+}
