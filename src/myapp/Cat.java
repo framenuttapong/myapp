bcb1b5c8018bea28c9e08a888c708bee3c595a18
Nuttapong Nadee <nuttapong.n@ku.th>
2022-07-04 02:02:48 +0700

    bring back src/myapp/Cat.java from commit a362f61

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,15 +3,9 @@ package myapp;
 public class Cat {
     String name;
     String type;
-    int age;
+
     public Cat(String name, String type) {
         this.name = name;
         this.type = type;
-    }
-
-    public Cat(String name, String type, int age) {
-        this.name = name;
-        this.type = type;
-        this.age = age;
     }
 }
