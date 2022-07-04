1b39b54ddf3d5b4e13dc42fab4ac6609f12e7a2a
Nuttapong Nadee <nuttapong.n@ku.th>
2022-07-04 12:01:03 +0700

    Update Cat.java by feature2

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -3,9 +3,15 @@ package myapp;
 public class Cat {
     String name;
     String type;
-
+    int age;
     public Cat(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public Cat(String name, String type, int age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
     }
 }
