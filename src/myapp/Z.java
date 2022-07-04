4ff785daf79337632cc2af2779d6490424e602d4
20♥ <nuttapong.n@ku.th>
2022-07-04 12:26:11 +0700

    update z

--- src/myapp/Z.java
+++ src/myapp/Z.java
@@ -2,8 +2,13 @@ package myapp;
 
 public class Z {
     int price;
-
+    String number;
     public Z(int price) {
         this.price = price;
+    }
+
+    public Z(int price, String number) {
+        this.price = price;
+        this.number = number;
     }
 }
