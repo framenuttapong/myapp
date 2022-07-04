aa79644141010a39f4b0f8e028a1208581c603c2
Nuttapong Nadee <nuttapong.n@ku.th>
2022-07-04 12:27:50 +0700

    update z too

--- src/myapp/Z.java
+++ src/myapp/Z.java
@@ -2,8 +2,13 @@ package myapp;
 
 public class Z {
     int price;
-
+    int age;
     public Z(int price) {
         this.price = price;
+    }
+
+    public Z(int price, int age) {
+        this.price = price;
+        this.age = age;
     }
 }
